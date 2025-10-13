e9b51fe3941cb30e1fa35c094d555cf4d6431ca1
Lacerda <samuell.rodri13@gmail.com>
2025-10-13 14:33:52 -0300

    Organização do Repositório

---
+++ Modulo 2/Banco de Dados/banco_dados2.py
@@ -0,0 +1,28 @@
+import sqlite3
+try:
+    con = sqlite3.connect("segundo_banco.db")
+    cur = con.cursor()
+
+    cur.execute("CREATE TABLE pessoa(" +
+                "id_pessoa INTEGER PRIMARY KEY ASC AUTOINCREMENT," +
+                "nome VARCHAR(400)," +
+                "idade INTEGER," +
+                "cpf VARCHAR(15))")
+
+    cur.execute("CREATE TABLE consulta(" +
+                "id INTEGER PRIMARY KEY ASC AUTOINCREMENT," +
+                "data_consulta DATETIME," +
+                "horario DATETIME," +
+                "id_pessoa INTEGER," +
+                "FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa))"
+                )
+
+    cur.execute("SELECT * FROM nome, data_consulta, WHERE pessoa.id_pessoa = %o" % 1)
+    res = cur.fetchone()
+    print(res)
+
+    con.commit()
+    cur.close()
+    con.close()
+except ConnectionRefusedError as c:
+    print('Erro de Conexão:', c)
